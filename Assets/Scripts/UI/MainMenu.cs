5eb369b33fe48dd2fcc937ce53c1207efd1d1556
NoneAndNever <43804135+NoneAndNever@users.noreply.github.com>
2021-11-16 16:55:08 +0800

    mainMenu完成

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+    public void UIEnabled()
+    {
+        GameObject.Find("/MainMenu/Image/UI").SetActive(true);
+    }
+}
